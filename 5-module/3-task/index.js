be4536416e5bda03af6e5d302eb6e7d6b39f2bb6
margarita2104 <margarita.p.ferreira@gmail.com>
2024-04-17 20:23:04 +0200

    3-я задача 5го модуля и задачи 6го модуля

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,41 @@
 function initCarousel() {
-  // ваш код...
+  let carouselInner = document.querySelector(".carousel__inner");
+  let buttonRight = document.querySelector(".carousel__arrow_right");
+  let buttonLeft = document.querySelector(".carousel__arrow_left");
+  let slideWidth = carouselInner.children[0].offsetWidth;
+
+  let currentSlideIndex = 0;
+
+  function updateNavigationButtons() {
+    if (currentSlideIndex === 0) {
+      buttonLeft.style.display = "none";
+    } else {
+      buttonLeft.style.display = "";
+    }
+
+    if (currentSlideIndex === carouselInner.children.length - 1) {
+      buttonRight.style.display = "none";
+    } else {
+      buttonRight.style.display = "";
+    }
+  }
+
+  updateNavigationButtons();
+
+  buttonRight.addEventListener("click", () => {
+    currentSlideIndex = Math.min(
+      currentSlideIndex + 1,
+      carouselInner.children.length - 1
+    );
+    carouselInner.style.transform =
+      "translateX(-" + currentSlideIndex * slideWidth + "px)";
+    updateNavigationButtons();
+  });
+
+  buttonLeft.addEventListener("click", () => {
+    currentSlideIndex = Math.max(currentSlideIndex - 1, 0);
+    carouselInner.style.transform =
+      "translateX(-" + currentSlideIndex * slideWidth + "px)";
+    updateNavigationButtons();
+  });
 }
--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,53 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.generateTable();
+  }
+
+  generateTable() {
+    let table = document.createElement("table");
+    let thead = document.createElement("thead");
+    let tbody = document.createElement("tbody");
+
+    let headers = ["Имя", "Возраст", "Зарплата", "Город", ""];
+    let headerRow = document.createElement("tr");
+    let headerCells = headers.map((headerText) => {
+      let th = document.createElement("th");
+      th.textContent = headerText;
+      return th;
+    });
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+
+    this.rows
+      .map((rowData) => {
+        let row = document.createElement("tr");
+        let cells = Object.values(rowData).map((value) => {
+          let cell = document.createElement("td");
+          cell.textContent = value;
+          return cell;
+        });
+        return { row, cells };
+      })
+      .forEach(({ row, cells }) => {
+        cells.forEach((cell) => row.appendChild(cell));
+
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = "X";
+        deleteButton.addEventListener("click", () => this.deleteRow(row));
+        let deleteCell = document.createElement("td");
+        deleteCell.appendChild(deleteButton);
+        row.appendChild(deleteCell);
+
+        tbody.appendChild(row);
+      });
+
+    table.appendChild(tbody);
+    return table;
+  }
+
+  deleteRow(row) {
+    row.remove();
   }
 }
