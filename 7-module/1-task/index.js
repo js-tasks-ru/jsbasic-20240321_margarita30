698d9b32cf4b98aa1fa0b641fdec6e28bce4edb3
margarita2104 <margarita.p.ferreira@gmail.com>
2024-04-23 15:31:19 +0200

    решение задач 7го модуля

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,97 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this.render();
+  }
+
+  render() {
+    this.elem = document.createElement("div");
+    this.elem.classList.add("ribbon");
+
+    const leftButton = document.createElement("button");
+    leftButton.classList.add(
+      "ribbon__arrow",
+      "ribbon__arrow_left",
+      "ribbon__arrow_visible"
+    );
+    leftButton.innerHTML =
+      '<img src="/assets/images/icons/angle-icon.svg" alt="icon">';
+    leftButton.addEventListener("click", () => this.scrollLeft());
+
+    const rightButton = document.createElement("button");
+    rightButton.classList.add("ribbon__arrow", "ribbon__arrow_right");
+    rightButton.innerHTML =
+      '<img src="/assets/images/icons/angle-icon.svg" alt="icon">';
+    rightButton.addEventListener("click", () => this.scrollRight());
+
+    const inner = document.createElement("nav");
+    inner.classList.add("ribbon__inner");
+
+    this.categories.forEach((category) => {
+      const item = document.createElement("a");
+      item.href = "#";
+      item.classList.add("ribbon__item");
+      item.dataset.id = category.id;
+      item.textContent = category.name;
+      item.addEventListener("click", (event) =>
+        this.selectCategory(event, category.id)
+      );
+      inner.appendChild(item);
+    });
+
+    this.elem.appendChild(leftButton);
+    this.elem.appendChild(inner);
+    this.elem.appendChild(rightButton);
+
+    document.body.appendChild(this.elem);
+  }
+
+  scrollLeft() {
+    const ribbonInner = this.elem.querySelector(".ribbon__inner");
+    ribbonInner.scrollBy(-350, 0);
+    this.toggleArrowsVisibility();
+  }
+
+  scrollRight() {
+    const ribbonInner = this.elem.querySelector(".ribbon__inner");
+    ribbonInner.scrollBy(350, 0);
+    this.toggleArrowsVisibility();
+  }
+
+  toggleArrowsVisibility() {
+    const ribbonInner = this.elem.querySelector(".ribbon__inner");
+    const scrollLeft = ribbonInner.scrollLeft;
+    const scrollWidth = ribbonInner.scrollWidth;
+    const clientWidth = ribbonInner.clientWidth;
+    const scrollRight = scrollWidth - scrollLeft - clientWidth;
+
+    const leftButton = this.elem.querySelector(".ribbon__arrow_left");
+    const rightButton = this.elem.querySelector(".ribbon__arrow_right");
+
+    leftButton.classList.toggle("ribbon__arrow_visible", scrollLeft > 0);
+    rightButton.classList.toggle("ribbon__arrow_visible", scrollRight > 1);
+  }
+
+  selectCategory(event, categoryId) {
+    event.preventDefault();
+
+    const currentItem = this.elem.querySelector(".ribbon__item_active");
+    if (currentItem) {
+      currentItem.classList.remove("ribbon__item_active");
+    }
+
+    const selectedItem = event.target;
+    selectedItem.classList.add("ribbon__item_active");
+
+    const selectEvent = new CustomEvent("ribbon-select", {
+      detail: categoryId,
+      bubbles: true,
+    });
+
+    this.elem.dispatchEvent(selectEvent);
   }
 }
+
+let ribbonMenu = new RibbonMenu(categories);
--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,4 +1,132 @@
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.value = value;
+    this.render();
+    this.initEventListeners();
+  }
+
+  render() {
+    this.elem = document.createElement("div");
+    this.elem.classList.add("slider");
+    this.elem.innerHTML = `
+      <div class="slider__thumb" style="left: ${this.value * (100 / (this.steps - 1))}%;">
+        <span class="slider__value">${this.value}</span>
+      </div>
+      <div class="slider__progress" style="width: ${this.value * (100 / (this.steps - 1))}%;"></div>
+      <div class="slider__steps">
+        ${Array.from({ length: this.steps })
+          .map(
+            (_, index) => `
+          <span ${index === this.value ? 'class="slider__step-active"' : ""}></span>
+        `
+          )
+          .join("")}
+      </div>
+    `;
+
+    let thumb = this.elem.querySelector(".slider__thumb");
+    thumb.ondragstart = () => false;
+  }
+
+  initEventListeners() {
+    const thumb = this.elem.querySelector(".slider__thumb");
+    thumb.addEventListener("pointerdown", this.onThumbPointerDown.bind(this));
+
+    this.elem.addEventListener("click", this.onSliderClick.bind(this));
+
+    document.addEventListener(
+      "pointermove",
+      this.onDocumentPointerMove.bind(this)
+    );
+    document.addEventListener("pointerup", this.onDocumentPointerUp.bind(this));
+  }
+
+  onThumbPointerDown(event) {
+    event.preventDefault();
+
+    this.elem.classList.add("slider_dragging");
+
+    this.updateThumbPosition(event);
+  }
+
+  onDocumentPointerMove(event) {
+    if (!this.elem.classList.contains("slider_dragging")) return;
+
+    this.updateThumbPosition(event);
+  }
+
+  onDocumentPointerUp(event) {
+    this.elem.classList.remove("slider_dragging");
+
+    this.elem.dispatchEvent(
+      new CustomEvent("slider-change", {
+        detail: this.value,
+        bubbles: true,
+      })
+    );
+  }
+
+  onSliderClick(event) {
+    const left = event.clientX - this.elem.getBoundingClientRect().left;
+    const leftRelative = left / this.elem.offsetWidth;
+
+    this.value = Math.round(leftRelative * (this.steps - 1));
+
+    this.updateThumbAndProgress();
+
+    this.elem.dispatchEvent(
+      new CustomEvent("slider-change", {
+        detail: this.value,
+        bubbles: true,
+      })
+    );
+  }
+
+  updateThumbPosition(event) {
+    const left = event.clientX - this.elem.getBoundingClientRect().left;
+    let leftRelative = left / this.elem.offsetWidth;
+
+    leftRelative = Math.max(0, Math.min(leftRelative, 1));
+
+    const thumb = this.elem.querySelector(".slider__thumb");
+    thumb.style.left = `${leftRelative * 100}%`;
+
+    const progress = this.elem.querySelector(".slider__progress");
+    progress.style.width = `${leftRelative * 100}%`;
+
+    this.value = Math.round(leftRelative * (this.steps - 1));
+
+    const valueDisplay = this.elem.querySelector(".slider__value");
+    valueDisplay.textContent = this.value;
+
+    const steps = this.elem.querySelectorAll(".slider__steps span");
+    steps.forEach((step, index) => {
+      if (index === this.value) {
+        step.classList.add("slider__step-active");
+      } else {
+        step.classList.remove("slider__step-active");
+      }
+    });
+  }
+
+  updateThumbAndProgress() {
+    const thumb = this.elem.querySelector(".slider__thumb");
+    thumb.style.left = `${this.value * (100 / (this.steps - 1))}%`;
+
+    const progress = this.elem.querySelector(".slider__progress");
+    progress.style.width = `${this.value * (100 / (this.steps - 1))}%`;
+
+    const valueDisplay = this.elem.querySelector(".slider__value");
+    valueDisplay.textContent = this.value;
+
+    const steps = this.elem.querySelectorAll(".slider__steps span");
+    steps.forEach((step, index) => {
+      if (index === this.value) {
+        step.classList.add("slider__step-active");
+      } else {
+        step.classList.remove("slider__step-active");
+      }
+    });
   }
 }
